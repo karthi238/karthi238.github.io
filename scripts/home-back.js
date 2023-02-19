60b8b4b73da800a53327b751807b856ef6b75652
Karthick Anbu <karthick.anbu@fintso.com>
2023-02-19 12:13:02 +0530

    code

--- scripts/home-back.js
+++ scripts/home-back.js
@@ -1,4 +1,4 @@
-var imageSources = ["./assets/images/home/image1.jpg", "./assets/images/home/image2.png", './assets/images/home/image3.png']
+var imageSources = ["./assets/images/home/image2.jpg", "./assets/images/home/image1.jpg", './assets/images/home/image3.jpg', './assets/images/home/image4.jpg', './assets/images/home/image5.jpg', './assets/images/home/image6.jpg', './assets/images/home/image7.jpg', './assets/images/home/image8.jpg']
 
 var index = 0;
 setInterval(function () {
